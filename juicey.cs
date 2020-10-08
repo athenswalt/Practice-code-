7847322b44b6b556e524af4e500964c41f4c17ed
Athens <69826578+athenswalt@users.noreply.github.com>
2020-10-08 16:14:55 -0500

    Create juicey.cs

---
+++ juicey.cs
@@ -0,0 +1,17 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.Write("hi do u use airpaids:");
+    string air = Console.ReadLine();
+    if (air.Equals("yes"))
+    {
+      Console.WriteLine("i hate you :(");
+    } 
+    else
+    {
+    Console.WriteLine("yay");
+    }
+    
+  }
+}
